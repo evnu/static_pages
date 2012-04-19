d8f09df11d581e0f47e294788eadefe6712e8165
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2012-04-19 10:54:16 +0200

    delete debug file

--- create_site.sh
+++ create_site.sh
@@ -59,7 +59,7 @@ FILES=$(for file in $@;
 do
     CREATION_DATE=$(head -n1 $file)
     echo $(generate_filename $file) $CREATION_DATE
-done | sed 's/%//g' | column -t | sort -k3 -k4M -k5 -k7 | tee hello | awk '{print $1}')
+done | sed 's/%//g' | column -t | sort -k3 -k4M -k5 -k7 | awk '{print $1}')
 
 NAV='<div id="sitenav"><ul>
 '
